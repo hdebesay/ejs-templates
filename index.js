7d0761d81a1c635ec79dac06aee5f4cc09efc93f
Conquering Fear <>
2022-08-17 05:32:27 +0000

    updated links

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+      
+
+app.get('/', (req, res) => {
+  var title = 'Our Home Page';
+  var heading = 'My website';
+  res.render('pages/index', {
+    'title': title,
+    'heading': heading
+  });
+})
+
+app.get('/about', (req, res) => {
+  var title = 'Our About Page';
+  var heading = "My website ";
+  res.render('pages/about', {
+    'title': title,
+    'heading': heading
+  });
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
